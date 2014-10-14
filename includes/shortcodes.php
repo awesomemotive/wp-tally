64ec3f4b9c8bd124cba82979824235f741150323
Sean Davis <sdavis2702@gmail.com>
2014-10-14 10:58:12 -0500

    add context for different plugin/rating situations

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -91,13 +91,15 @@ function wptally_shortcode( $atts, $content = null ) {
                     }
                 }
                 
+                $plugins_total = number_format( $count );
+                $plugins_reference = $plugins_total == 1 ? 'plugin' : 'plugins';
                 $cumulative_rating = $ratings_total / $ratings_count;
 
                 // Totals row
                 $results .= '<div class="tally-plugin">';
                 $results .= '<div class="tally-plugin-left">';
                     $results .= '<div class=tally-info">';
-                        $results .= '<p class=tally-count-rating">You have <span class=tally-count">' . number_format( $count ) . '</span> plugins with a cumulative rating of <span class=tally-rating">' . number_format( $cumulative_rating, 2, '.', '' ) . '</span> out of 5 stars</p>';
+                        $results .= '<p class=tally-count-rating">You have <span class=tally-count">' . $plugins_total . '</span> ' . $plugins_reference . ( empty( $ratings_count ) ? ' with no ratings.' : ' with a cumulative rating of <span class=tally-rating">' . number_format( $cumulative_rating, 2, '.', '' ) . '</span> out of 5 stars.</p>' );
                     $results .= '</div>';
                     $results .= '<div class=tally-share">';
                         $results .= '<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://wptally.com/?wpusername=' . esc_attr( $username ) . '" data-text="My plugins on WordPress.org have a total of ' . number_format( $total_downloads ) . ' downloads. Check it out on wptally.com">Tweet</a>
